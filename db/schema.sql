2d6df0ce70443ddddf6e38f74a7513bcea584b18
Miriam Armada <miriam_armada@yahoo.com>
2018-05-15 13:19:03 -0700

    SQL DB Schema and Seeds data

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,24 @@
 ### Schema
+DROP DATABASE IF EXISTS dietCheater_db;
+CREATE DATABASE dietCheater_db;
+USE dietCheater_db;
 
-CREATE DATABASE project2_db;
-USE project2_db;
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  weight-lb DECIMAL(10,2) NOT NULL,
+  weight-kg DECIMAL(10,2) DEFAULT (COALESCE(weight-lb,0)*.45392),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE exercise (
+    id INT NOT NULL AUTO_INCREMENT,
+    x_name VARCHAR(50) NULL,
+    male_factor DECIMAL(10,6) NOT NULL
+    female_factor DECIMAL(10,6) NOT NULL,
+)
+
 
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE dietCheater_db;
+INSERT INTO exercise (x_name, male_factor, female_factor)
+VALUES ("Burpee", 0.0105, 0.0095);
+
+USE dietCheater_db;
+INSERT INTO users (user_name, name, lastname, email, weight-lb, )
+VALUES ("max1987", "Max", "Yorkie", "maxtyorkie@gmail.com", 13 );
