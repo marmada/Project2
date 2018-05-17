6c253f20608e05f1a730b8eecbeed3efea608370
Miriam Armada <miriam_armada@yahoo.com>
2018-05-17 15:19:05 -0700

    update api files

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,27 @@
-var express = require("express");
+var express = require('express');
 
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var main = require("../models/main.js");
+var main = require('../models/main.js');
+var login = require('../models/login.js');
+var auth = require('../models/main.js');
 
 // ENTER ROUTES HERE
+
+// Route to Post login information from AJAX to "LOGIN"
+router.post('/api/login', function(req, res) {
+  var userInfo = req.body;
+  console.log(req.body);
+
+});
+
+
+router.post('/api/register', function(req, res) {
+    var userInfo = req.body;
+    console.log(req.body);
+  
+  });
+  
 // Export routes for server.js to use.
 module.exports = router;
--- public/assets/js/action.js
+++ public/assets/js/action.js
@@ -1,44 +1,73 @@
 
-require ('dotenv').config ();
 
 $(function() {
-  $("#search").on("submit", function(event) {
+
+  // function 1 to grabs "search" for user and GET Nutrition data
+
+  $('#search').on('submit', function(event) {
     event.preventDefault();
-    
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
 
-    var newSleepState = {
-      sleepy: newSleep
-    };
+    var query = $('#search').val().trim();
+    var apiKey = process.env.APIKEY;
 
+    //var apikey = "R8gYygJ9lQqhfgowOnDP2afcsq5YM7tRNp7cPqTl";
 
-var query= 'hersheys';
-//var apiKey = process.env.APIKEY;
-var apikey = "R8gYygJ9lQqhfgowOnDP2afcsq5YM7tRNp7cPqTl";
+    console.log(apikey);
 
-console.log(apikey);
+    var queryURL =
+      'https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=10&offset=0&api_key=' +
+      apikey +
+      '&q=';
+    var search = queryURL + query;
+    console.log(queryURL + query);
 
-
-var queryURL = "https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=10&offset=0&api_key=" + apikey + "&q=";
-
-console.log(queryURL+query);
-
-function searchFood(){
-
-  $.ajax({
-    url: queryURL+query,
-    method: "GET"
-    }).then(function(response) {   
-
+    $.ajax({
+      url: search,
+      method: 'GET',
+    }).then(function(response) {
       console.log(response);
-     // var data = JSON.parse(response);
 
-      console.log(response.data);
+      var items = JSON.parse(response.list.item);
+
+      //For Loop to run 10 images through API Object
+      for (var i = 0; i < items.length; i++) {
+        //Setting Div to hold Image & Rating
+        var foodchoices = $("<div class='newDiv'>");
+
+        var food = items[i].name;
+        var id = items[i].ndbno;
+      }
+    });
+  });
+
+  // Ajax to grab information from user login input for validation
+
+  $('#loginS').on("click", function(event) {
+
+    event.preventDefault();
+
+    // get the form data & validate is not "blank"
+  
+      var loginData = {
+        user: $('#email').val().trim(),
+        password: $('#password').val().trim()
+      };
+      console.log(loginData);
+
+      if (loginData.user != '' && loginData.password != '') {
+
+      // process the form
+      $.ajax('/login', {
+        type: 'POST',
+        data: loginData,
+      }).done(function() {
+        console.log('info to route for validation succesful');
+      });
+    } else {
+      msg = 'Invalid username and password!';
+
+      $('#message').html(msg);
+    }
+  });
   
 });
-};
-
-  });
-});
-
