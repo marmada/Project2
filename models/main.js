7c2e12ecf7f884dd8d9917d8a6268f196d0c29ac
Miriam Armada <miriam_armada@yahoo.com>
2018-05-19 01:16:38 -0700

    api nutrition instant search

--- models/main.js
+++ models/main.js
@@ -1,7 +1,42 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var main ={};
+
+// Import the ORM to create functions that will interact with the database.
+
+var dietCheater ={};
+
+    // Import the ORM to create functions that will interact with the database.
+
+  all: function(table, cb) {
+    orm.all(table, function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(table, cols, vals, cb) {
+    orm.create(table, cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(table, objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(table, condition, cb) {
+    orm.delete(table, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (controller.js).
+module.exports = dietCheater;
+
+
+
+
 
 
 // Export the database functions for the controller controller.js).
--- public/assets/js/action.js
+++ public/assets/js/action.js
@@ -1,10 +1,53 @@
-
+//require('dotenv').config();
 
 $(function() {
+ 
 
-  // function 1 to grabs "search" for user and GET Nutrition data
+  //var apikey = process.env.APIKEY;
+ // console.log(apikey);
 
-  $('#search').on('submit', function(event) {
+    // function 1 to grabs "search" for user and GET Nutrition data
+  $.ajaxSetup({ cache: false });
+  $('#search').keyup(function() {
+    $('#result').html('');
+    var q = $('#search').val();
+    var resultDropdown = $('#result');
+    console.log(q);
+
+   var apikey = 'R8gYygJ9lQqhfgowOnDP2afcsq5YM7tRNp7cPqTl';
+
+    var queryURL =
+      'https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=25&offset=0&api_key=' +
+      apikey +
+      '&q=' + q;
+
+    console.log(queryURL);
+
+    $.getJSON({
+      url: queryURL,
+      method: 'GET',
+    }).done(function(response) {
+
+      var options= response.list.item;
+
+//loop thru response
+
+      for (i = 0; i < options.length; i++){
+
+        var product =options[i].name.split(',');
+        var item = $('<li>')
+        item.addClass("list-group-item link-class");
+        item.attr("id", options[i].ndbno);
+        item.text(product[0]);
+
+        resultDropdown.append(item);
+
+           }
+      });
+    });
+  
+
+  /*$('#search').on('submit', function(event) {
     event.preventDefault();
 
     var query = $('#search').val().trim();
@@ -15,7 +58,7 @@ $(function() {
     console.log(apikey);
 
     var queryURL =
-      'https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=20&offset=0&api_key=' +
+      'https://api.nal.usda.gov/ndb/search/?format=json&sort=r&max=10&offset=0&api_key=' +
       apikey +
       '&q=';
     var search = queryURL + query;
@@ -29,7 +72,7 @@ $(function() {
 
       var items = JSON.parse(response.list.item);
 
-      //For Loop to add 10 objects to dropdown menu
+      //For Loop to run 10 images through API Object
       for (var i = 0; i < items.length; i++) {
         //Setting Div to hold Image & Rating
         var foodchoices = $("<div class='newDiv'>");
@@ -38,30 +81,28 @@ $(function() {
         var id = items[i].ndbno;
       }
     });
-  });
+  });*/
 
   // Ajax to grab information from user login input for validation
 
-  $('#loginS').on("click", function(event) {
-
+  $('#loginS').on('click', function(event) {
     event.preventDefault();
 
     // get the form data & validate is not "blank"
-  
-      var loginData = {
-        user: $('#email').val().trim(),
-        password: $('#password').val().trim()
-      };
-      console.log(loginData);
 
-      if (loginData.user != '' && loginData.password != '') {
+    var loginData = {
+      user: $('#email').val().trim(),
+      password: $('#password').val().trim(),
+    };
+    console.log(loginData);
 
+    if (loginData.user != '' && loginData.password != '') {
       // process the form
       $.ajax({
-        url:'/login', 
+        url: '/login',
         type: 'POST',
         data: loginData,
-        dataType:'JSON'
+        dataType: 'JSON',
       }).done(function() {
         console.log('info to route for validation succesful');
       });
@@ -72,34 +113,33 @@ $(function() {
     }
   });
 
-
-  // Ajax to grab information from registration page input, creates user in database
-  
-  $('#register').on("click", function(event) {
-
+  $('#register').on('click', function(event) {
     event.preventDefault();
 
     // get the form data & validate is not "blank"
-  
-      var registerData = {
-        user: $('#email-2').val().trim(),
-        password: $('#password-2').val().trim(),
-        nickname: $('#nickname').val().trim(),
-        name: $('#name').val().trim(),
-        lastname: $('#lastname').val().trim(),
-        weight: $('#weight').val().trim(),
-        height: parseInt($('#height-ft').val())*12+parseInt($("#height-in"))
-      };
-      console.log(loginData);
 
-      if (registerData.user != '' && registerData.password != '' && registerData.nickname!='') {
+    var registerData = {
+      email: $('#email-2').val().trim(),
+      password: $('#password-2').val().trim(),
+      nickname: $('#nickname').val().trim(),
+      name: $('#name').val().trim(),
+      lastname: $('#lastname').val().trim(),
+      weight: $('#weight').val().trim(),
+      height: parseInt($('#height-ft').val()) * 12 + parseInt($('#height-in')),
+    };
+    console.log(loginData);
 
+    if (
+      registerData.user != '' &&
+      registerData.password != '' &&
+      registerData.nickname != ''
+    ) {
       // process the form
       $.ajax({
-        url:'/register', 
+        url: '/register',
         type: 'POST',
         data: registerData,
-        dataType:'JSON'
+        dataType: 'JSON',
       }).done(function() {
         console.log('info to route for registration succesful');
       });
@@ -109,6 +149,4 @@ $(function() {
       $('#message').html(msg);
     }
   });
-  
-  
 });
